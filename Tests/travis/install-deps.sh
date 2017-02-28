e07856ab156283b8c0a4fd62666c2f2aa969652d
Michael <Bruce17@users.noreply.github.com>
2017-02-28 10:45:44 +0100

    Update install-deps.sh

--- Tests/travis/install-deps.sh
+++ Tests/travis/install-deps.sh
@@ -2,13 +2,17 @@
 
 BASEDIR=$(dirname $0);
 
+echo "travis version: $TRAVIS_PHP_VERSION";
+PHP_ENV_VERSION=$(phpenv version-name);
+echo "php env version: $PHP_ENV_VERSION";
+
 if [ "$TRAVIS_PHP_VERSION" = "hhvm" ]; then
     exit 0;
 fi
 
 pecl install riak
 
-if [ "$TRAVIS_PHP_VERSION" ~= '7\.\d+' ]; then
+if [ "$TRAVIS_PHP_VERSION" =~ '7\.\d+' ]; then
     pecl install mongo
 
     phpenv config-add $BASEDIR/php7.ini
